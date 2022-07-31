1d3b89ca4db315fce99904fd22d510eb75dd71de
andreea <andreeaalexandrache21@gmail.com>
2022-07-31 20:20:34 +0300

    Scss to css changes

--- src/index.tsx
+++ src/index.tsx
@@ -8,19 +8,27 @@ import {H1} from "./components/Heading/H1";
 import {H2} from "./components/Heading/H2";
 import {Text} from "./components/Text/Text";
 import {TextType} from "./components/Text/types";
+import {ToggleFilter} from "./components/ToggleFilter/ToggleFilter";
+import {ToggleFilterElementType, ToggleFilterType} from "./components/ToggleFilter/types";
+import {Spacer} from "./components/Spacer/Spacer";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const children: TextType[] = [
-    {children: "text2"},
+const filters: ToggleFilterElementType[] = [
+    {isActive: true, label: "filtru", name:"test"},
+    {isActive: true, label: "filtru2", name:"test"}
+
 ];
+
 
 root.render(
   <React.StrictMode>
     <div>
-        <Text>Test</Text>
+        <ToggleFilter filters={filters}/>
+        <Spacer/>
+        <ToggleFilter filters={filters}/>
     </div>
   </React.StrictMode>
 );
