ae3e00287b5c97c53a8734e8bc282cd3772e8cc5
andreea <andreeaalexandrache21@gmail.com>
2022-07-31 19:20:38 +0300

    Scss to css changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Select} from "./components/Select/Select";
 import {SelectItemType} from "./components/Select/types";
+import {Button} from "./components/Button/Button";
+import {Checkbox} from "./components/Checkbox/Checkbox";
+import {H1} from "./components/Heading/H1";
+import {H2} from "./components/Heading/H2";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const items: SelectItemType[] = [
-    {label: "Item 1", name: "item1", isChecked: false},
-    {label: "Item 2", name: "item2", isChecked: false},
-    {label: "Item 3", name: "item3", isChecked: false},
-    {label: "Item 4", name: "item4", isChecked: true},
-    {label: "Item 5", name: "item5", isChecked: false},
-    {label: "Item 6", name: "item6", isChecked: false},
-];
 
 root.render(
   <React.StrictMode>
     <div>
-        <Select items={items} label={"Select a value"} isOpen={true}/>
+        <H2 variant={"secondary"}>Test</H2>
     </div>
   </React.StrictMode>
 );
