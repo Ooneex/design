11041dbda8e351e24d2552814598e7b6cd07ae27
andreea <andreeaalexandrache21@gmail.com>
2022-08-01 14:49:05 +0300

    feat(component): create icon component for: -WindowCode -Discord -GitHub -Twitter -WarningSign -Copy2 -Bulb63

---
+++ src/components/Icon/IconWindowCode.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Icon} from "./Icon";
+import {IconType} from "./types";
+
+export const IconWindowCode = ({variant = "primary", size = "normal", spin}: IconType) => {
+    return (
+        <Icon>
+            <i className={"_ooneex-font-icon _icon-window-code " + variant + " " + size + (spin ? " _ooneex-font-icon-is-spinning" : "")} />
+        </Icon>
+    );
+};
