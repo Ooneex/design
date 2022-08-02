0bc815a0f8800a7cf22434d243559775ceb77faa
andreea <andreeaalexandrache21@gmail.com>
2022-08-03 00:17:15 +0300

    feat(component):-first version of terminal component -create new colors for the component

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -3,29 +3,36 @@ import "./Terminal.css";
 import * as React from 'react';
 import {IconWindowCode} from "../Icon/IconWindowCode";
 import {Spacer} from "../Spacer/Spacer";
+import {IconArrowAltRight} from "../Icon/IconArrowAltRight";
+import {IconCopy2} from "../Icon/IconCopy2";
 
 export const Terminal= ({code}: TerminalType) => {
 
     return (
         <div className={"_ooneex-terminal"}>
-            <IconWindowCode size={"md"} />
-
             <div className={"header"}>
                 <span className={"dot-container"}>
                     <span className={"dot-red"} />
                     <span className={"dot-yellow"}/>
                     <span className={"dot-green"}/>
                 </span>
-                <span className={"title-header"}>
-                    ~/my/projects
-                </span>
+                <div className={"title-header"}>
+                    <span className={"icon-terminal"}><IconWindowCode  variant={"windowcode"} size={"md"}/></span>
+                  <span className={"title-text"}>~/my/projects</span>
+                </div>
             </div>
-            <Spacer size={"xs"}/>
-            ~/my/projects
-            <div className={"window-code"}>
-                <IconWindowCode size={"md"} variant={"white"}/>
-              <code>my code here</code>
+            <div className={"inside-container"}>
+            <span className={"title-text-second"}>~/my/projects</span>
+            <span className={"window-code"}>
+                <span className={"arrow-group"}>
+                <IconArrowAltRight size={"xs"} variant={"arrow"}/>
+                <span className={"arrow-toleft"}><IconArrowAltRight size={"xs"} variant={"arrow"}/></span>
+                <span className={"arrow-toleft"}><IconArrowAltRight size={"xs"} variant={"arrow"}/></span>
+            </span>
+                <span className={"ooneex-code"}><span className={"deno-word"}>deno</span><code>task project:build</code></span>
+            </span>
             </div>
+            <span className={"icon-copy"}><IconCopy2  size={"md"} variant={"disabled"}></IconCopy2></span>
         </div>
 
     )
