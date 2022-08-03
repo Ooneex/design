4782d475c8f67757dae5b424e4b7d663e6c51fe0
andreea <andreeaalexandrache21@gmail.com>
2022-08-03 22:49:38 +0300

    feat(component):-create youtube container

---
+++ src/components/YoutubeContainer/types.ts
@@ -0,0 +1,3 @@
+export type YoutubeContainerType = {
+    text: string;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Terminal} from "./components/Terminal/Terminal";
 import "./index.scss";
+import {YoutubeContainer} from "./components/YoutubeContainer/YoutubeContainer";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <div className={"app"}>
-        <Terminal code={"deno task run"} title={"~/documents"} />
+        <YoutubeContainer text={"bkj"} />
     </div>
   </React.StrictMode>
 );
